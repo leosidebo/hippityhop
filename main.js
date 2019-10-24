4ea0767de1b1046e836ebb5cffe25f0ac1bbdccc
leosidebo <abletoirper@gmail.com>
2019-10-24 15:06:33 +0200

    Moved functions from main.js to a player class

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const size = 128;
 const scaledSize = scale * size;
 const cycleLoopMove = [0, 1, 2, 3, 2, 1, 0];
 const cycleLoopJump = [0, 3, 2, 1];
-const movement_speed = 1.25;
 const facing_right = 0;
 const facing_left = 2;
 const frame_limit_move = 5;
@@ -18,18 +17,15 @@ canvas.height = 550;
 let currentLoopIndex = 0;
 let frameCount = 0;
 let keyPresses = {};
-let positionX = (canvas.width / 2) - scaledSize;
+let positionX = canvas.width / 2 - scaledSize;
 let positionY = 0;
-let currentDirection = facing_right;
-let img = new Image();
-let gravity = 0.1;
-let gravitySpeed = 0;
-let hitBottom = false;
-let jumpForceUp = 0;
+let playerImg = new Image();
+let gravity = 0.02;
 let jumpForceLeft = 0;
 let jumpForceRight = 0;
 let jumpInProgress = false;
 
+let player = new Player(positionX, positionY);
 
 /**
  * Marks the key pressed as true in keyPresses.
@@ -51,89 +47,55 @@ function keyUpListener(event) {
  * Loads the image for the player and animates it.
  */
 function loadImage() {
-    img.src =
-        'images/PlayerSprite2.png';
-    img.onload = function() {
-        drawFrame(
+    playerImg.src = 'images/PlayerSprite2.png';
+    playerImg.onload = function() {
+        player.draw(
             cycleLoopMove[currentLoopIndex],
-            currentDirection,
-            positionX,
-            positionY
+            player.currentDirection
         );
-        window.requestAnimationFrame(gameLoop);  
+        window.requestAnimationFrame(gameLoop);
     };
-}
-
-/**
- * Function that uses drawImage but simplifies input.
- * @param {*} frameX 
- * @param {*} frameY 
- * @param {*} canvasX 
- * @param {*} canvasY 
- */
-function drawFrame(frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(
-        img,
-        frameX * size,
-        frameY * size,
-        size,
-        size,
-        canvasX,
-        canvasY,
-        scaledSize,
-        scaledSize
-    );
 }
 
 loadImage();
 
 /**
- * The main gameloop that executes every frame. 
+ * The main gameloop that executes every frame.
  * Animates vertical movement upon valid keypresses.
  */
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(cycleLoopMove[currentLoopIndex], player.currentDirection);
 
     let hasMoved = false;
-    gravitySpeed += gravity;
+    player.update();
 
-    // Applies gravity if the player has not hit the bottom of the canvas.
-    if (!hitBottom) {
-        gravityMove(gravitySpeed);
-    }
-
-    else {
+    if (player.hitBottom) {
         if (keyPresses.a) {
-            moveCharacter(-movement_speed, facing_right);
+            player.moveX(-player.movement_speed, facing_right);
             hasMoved = true;
-        } 
-        
-        else if (keyPresses.d) {
-            moveCharacter(movement_speed, facing_left);
+        } else if (keyPresses.d) {
+            player.moveX(player.movement_speed, facing_left);
             hasMoved = true;
-        } 
-        
-        if(keyPresses.w) {
+        }
 
+        if (keyPresses.w) {
             jumpInProgress = true;
             if (keyPresses.a && jumpForceLeft == 0) {
-                if (jumpForceRight < 50) {
+                if (jumpForceRight < 75) {
                     jumpForceRight++;
                 }
-            } 
-
-            else if (keyPresses.d && jumpForceRight == 0) {
-                if (jumpForceLeft < 50) {
+            } else if (keyPresses.d && jumpForceRight == 0) {
+                if (jumpForceLeft < 75) {
                     jumpForceLeft++;
                 }
             }
-            
-            if (jumpForceUp < 50) {
+
+            if (jumpForceUp < 75) {
                 jumpForceUp++;
             }
-        } 
-
-        else if (jumpInProgress) {
+        } else if (jumpInProgress) {
+            velocityY = jumpForceUp;
             jump(jumpForceUp, jumpForceLeft, jumpForceRight);
         }
     }
@@ -143,57 +105,11 @@ function gameLoop() {
         if (frameCount >= frame_limit_move) {
             frameCount = 0;
             currentLoopIndex++;
-            if (currentLoopIndex >= cycleLoopMove.length) {
-                currentLoopIndex = 0;
-            }
+            currentLoopIndex %= cycleLoopMove.length;
         }
+
+        player.draw(cycleLoopMove[currentLoopIndex, player.currentDirection])
     }
 
-    drawFrame(cycleLoopMove[currentLoopIndex], currentDirection, positionX, positionY);
     window.requestAnimationFrame(gameLoop);
-}
-/**
- * Makes the player fall to the ground.
- * @param {gravitySpeed} deltaY 
- */
-function gravityMove(deltaY) {
-    if (positionY + scaledSize + deltaY < canvas.height) {
-        positionY += deltaY;
-    } else {
-        hitBottom = true;
-    }
-}
-
-/**
- * Moves the player horizontally. Cannot go past the borders of the canvas.
- * @param {movement_speed} deltaX 
- * @param {*} direction 
- */
-function moveCharacter(deltaX, direction) {
-    if (
-        positionX + deltaX > 0 &&
-        positionX + scaledSize + deltaX < canvas.width
-    ) {
-        positionX += deltaX;
-    }
-
-    currentDirection = direction;
-}
-
-/**
- * Allows the player to jump.
- * @param {*} deltaY 
- * @param {*} deltaL 
- * @param {*} deltaR 
- */
-function jump(deltaY, deltaL, deltaR) {
-    
-    let jumpAnimation = setInterval(() => {
-        positionY -= deltaY;
-    }, 0.25);
-    hitBottom = false;
-    jumpForceUp = 0;
-    jumpForceLeft = 0;
-    jumpForceRight = 0;
-    jumpInProgress = false;
 }
---
+++ player.js
@@ -0,0 +1,58 @@
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.hitBottom = false;
+        this.jumpForceUp = 0;
+        this.gravitySpeed = 0;
+        this.movement_speed = 1.25;
+        this.currentDirection = facing_right;
+    }
+
+    update() {
+        if (this.gravitySpeed < 0.2 && !this.hitBottom) {
+            this.gravitySpeed += gravity;
+        }
+
+        if (this.y + scaledSize + this.gravitySpeed > canvas.height) {
+            this.hitBottom = true;
+            this.gravitySpeed = 0;
+            this.velocityY = this.gravitySpeed;
+        }
+
+        if (!this.hitBottom) {
+            this.velocityY += this.gravitySpeed;
+        }
+        
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+        
+    }
+
+    draw(frameX, frameY) {
+        ctx.drawImage(
+            playerImg,
+            frameX * size,
+            frameY * size,
+            size,
+            size,
+            this.x,
+            this.y,
+            scaledSize,
+            scaledSize
+        )
+    }
+
+    moveX(deltaX, direction) {
+        if (
+            this.x + deltaX > 0 &&
+            this.x + scaledSize + deltaX < canvas.width
+        ) {
+            this.x += deltaX;
+        }
+
+        this.currentDirection = direction;
+    }
+}
