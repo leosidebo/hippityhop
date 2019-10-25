0b6be5e22bf54c7dd2fed8b72968c887755d75b8
leosidebo <abletoirper@gmail.com>
2019-10-25 09:58:00 +0200

    Comments and horizontal jumping

--- player.js
+++ player.js
@@ -5,20 +5,29 @@ class Player {
         this.velocityX = 0;
         this.velocityY = 0;
         this.hitBottom = false;
-        this.jumpForceUp = 0;
+        this.jumpForceUp = 3;
+        this.jumpForceLeft = 0;
+        this.jumpForceRight = 0;
         this.gravitySpeed = 0;
         this.movement_speed = 1.25;
         this.currentDirection = facing_right;
     }
 
+    /**
+     * The main gameloop for the player. Called every frame. 
+     * Applies gravity, checks for bottom bounds and moves the player.
+     */
     update() {
+        // Accelerates the speed of gravity, up to a point.
         if (this.gravitySpeed < 0.2 && !this.hitBottom) {
             this.gravitySpeed += gravity;
         }
 
+        // 
         if (this.y + scaledSize + this.gravitySpeed > canvas.height) {
             this.hitBottom = true;
             this.gravitySpeed = 0;
+            this.velocityX = 0;
             this.velocityY = this.gravitySpeed;
         }
 
@@ -26,10 +35,33 @@ class Player {
             this.velocityY += this.gravitySpeed;
         }
 
+        /* if (
+            this.x - this.velocityX > 0 &&
+            this.x + scaledSize + this.velocityX < canvas.width
+        ) {
+            console.log("jam")
+            this.velocityX = 0;
+            this.velocityY = this.gravitySpeed;
+        }
+
+        if (
+            this.y - this.velocityY > 0 &&
+            this.y + scaledSize + this.velocityY < canvas.height
+        ) {
+            console.log("bill")
+            this.velocityX = 0;
+            this.velocityY = this.gravitySpeed;
+        } */
+
         this.x += this.velocityX;
         this.y += this.velocityY;
     }
 
+    /**
+     * Draws the player and simplifies the input.
+     * @param {*} frameX 
+     * @param {*} frameY 
+     */
     draw(frameX, frameY) {
         ctx.drawImage(
             playerImg,
@@ -44,6 +76,11 @@ class Player {
         );
     }
 
+    /**
+     * Moves the player horizontally.
+     * @param {*} deltaX 
+     * @param {*} direction 
+     */
     moveX(deltaX, direction) {
         if (
             this.x + deltaX > 0 &&
@@ -55,12 +92,20 @@ class Player {
         this.currentDirection = direction;
     }
 
-    jump(forceUp) {
-        console.log(forceUp);
+    /**
+     * Allows the player to jump either straight up or up with a direction.
+     * @param {*} forceUp 
+     * @param {*} forceRight 
+     * @param {*} forceLeft 
+     */
+    jump(forceUp, forceRight, forceLeft) {
         this.velocityY -= forceUp;
+        this.velocityX = this.velocityX + forceLeft - forceRight;
         this.y += this.velocityY;
         this.hitBottom = false;
-        this.jumpForceUp = 0;
+        this.jumpForceUp = 3;
+        this.jumpForceLeft = 0;
+        this.jumpForceRight = 0;
         jumpInProgress = false;
     }
 }
